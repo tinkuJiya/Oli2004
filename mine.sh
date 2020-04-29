c7c20d63233b1aeb7296030d150e1f2fa711f1fc
Oli2004 <30935873+Oli2004@users.noreply.github.com>
2020-04-29 16:17:06 -0400

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.microsoftAzure_miner_lol -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u zs1j4u383ujxzjruecaq8pxz8sgufds6y23gw7qmf67kly8lp757e9zm42t3lvhg7kyw2gsxue3esk.microsoftAzure_miner_lol -p x --cpu 4
