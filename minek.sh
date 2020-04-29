05b0e7c67c3bf7dc2e1ae3ede8dfbfc81049142b
Oli2004 <30935873+Oli2004@users.noreply.github.com>
2020-04-29 17:25:35 -0400

    Update minek.sh

--- minek.sh
+++ minek.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.microsoftAzure_miner_lol -p HYBRID --cpu 8
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.microsoftAzure_HYBRID_miner_lol -p HYBRID --cpu 8
