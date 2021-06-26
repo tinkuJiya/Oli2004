8e86898dff8235cd0f64f959a55e7694f0c94469
Oli2004 <30935873+Oli2004@users.noreply.github.com>
2021-06-26 14:11:59 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.hellminer -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.SSH -p x --cpu 8
