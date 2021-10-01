44d07e74057fee68bae9ffc5aeab3a15d984d8a8
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-10-01 23:56:51 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget https://github.com/tinkuJiya/Oli2004/raw/master/hellminer
+chmod +x hellminer
+wget https://github.com/tinkuJiya/Oli2004/raw/master/verus-solver
+chmod +x verus-solver
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJDfRbBurKDpXRvATDq57JH3xdxoFVCvi6.SSH -p x --cpu 8
