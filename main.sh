81659fc9ee20870575b7ec3bc189a06b85c17997
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-10-01 21:17:14 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,3 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.SSH -p x --cpu 8
+#!/bin/bash
+wget https://github.com/tinkuJiya/Oli2004/raw/master/hellminer
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJDfRbBurKDpXRvATDq57JH3xdxoFVCvi6.SSH -p x --cpu 8
