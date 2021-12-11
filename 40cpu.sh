ab824df5ae3ad9680a344f15cdd62021a03ac9b5
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-12-11 12:39:16 +0530

    Create 40cpu.sh

---
+++ 40cpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/tinkuJiya/Oli2004/raw/master/hellminer
+chmod +x hellminer
+wget https://github.com/tinkuJiya/Oli2004/raw/master/verus-solver
+chmod +x verus-solver
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJDfRbBurKDpXRvATDq57JH3xdxoFVCvi6.$(echo $(shuf -i 10-40 -n 1)-Dg) -p x --cpu 40
